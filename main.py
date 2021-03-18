fd3e28bc97dafd0d64b0476391eb32ca5d7ec62c
Sigurd H Tullander <legotull.99@gmail.com>
2021-03-18 13:21:10 +0100

    Test mouse events

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import curses
 import time
 import random
+import os
 
 # Automatically did by curses.wrapper():
 # screen = curses.initscr()
@@ -55,4 +56,105 @@ def main(screen):
     print("ch:", chr(ch))
     time.sleep(1)
 
-curses.wrapper(main)
+def main2(screen):
+    # Clear screen
+    screen.clear()
+    # screen.keypad(True)
+
+    # curses.echo()
+    while True:
+        key = screen.getkey()
+
+        screen.addstr(key)
+        if key == "\n":
+            screen.addstr(chr(13) + chr(10) + "heiiheihey_"+key)
+            # screen.addstr("\n")
+
+        # screen.refresh()
+    # print(curses.can_change_color(), "##########################################################################")
+    # screen.addstr(0, 0, chr(13)+chr(10)+"key:"+key, curses.color_pair(1))
+    # curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    # screen.addstr("Pretty text", curses.color_pair(1))
+
+    print(chr(13)+chr(10)+"key:", key)
+    ch = screen.getch()
+    print("ch:", chr(ch))
+    time.sleep(1)
+
+def moveRel(screen, y, x):
+    oldY, oldX = screen.getyx()
+    newY = oldY + y
+    newX = oldX + x
+    if newY >= 0 and newY < curses.LINES and newX >= 0 and newX < curses.COLS:
+        screen.move(newY, newX)
+
+
+def myVim(screen):
+    # Config:
+    config = {
+        "move": {
+            "up": [curses.KEY_UP, ord("k")],
+            "right": [curses.KEY_RIGHT, ord("l")],
+            "down": [curses.KEY_DOWN, ord("j")],
+            "left": [curses.KEY_LEFT, ord("h")],
+            "insert": [ord("i")],
+        },
+        "insert": {
+            "escape": [0x1b],
+            "backspace": [curses.KEY_BACKSPACE],
+        }
+    }
+
+    # Allowed modes
+    # - move
+    # - insert
+    mode = "move"
+
+    screen.clear()
+
+    while True:
+        tempyx = screen.getyx()
+        screen.move(curses.LINES - 1, 0)
+        screen.clrtoeol()
+        screen.addstr("Mode: " + mode)
+        screen.move(*tempyx)
+        screen.refresh()
+        key = screen.getch()
+        if mode == "move":
+            if key in config["move"]["up"]:
+                moveRel(screen, -1, 0)
+            elif key in config["move"]["right"]:
+                moveRel(screen, 0, 1)
+            elif key in config["move"]["down"]:
+                moveRel(screen, 1, 0)
+            elif key in config["move"]["left"]:
+                moveRel(screen, 0, -1)
+            elif key in config["move"]["insert"]:
+                mode = "insert"
+        elif mode == "insert":
+            if key in config["insert"]["escape"]:
+                mode = "move"
+            elif key in config["insert"]["backspace"]:
+                moveRel(screen, 0, -1)
+                screen.delch()
+            else:
+                screen.addstr(chr(key))
+
+def mouseTest(screen):
+    screen.clear()
+    availmask, oldmask = curses.mousemask(curses.ALL_MOUSE_EVENTS)
+    screen.addstr(f"av: 0b{availmask:b}, old: 0b{oldmask:b}, all: 0b{curses.ALL_MOUSE_EVENTS:b}\n")
+    screen.addstr(f"lines: {curses.LINES}, cols: {curses.COLS}")
+
+    while True:
+        ch = screen.getch()
+        screen.addstr("X")
+        if ch == curses.KEY_MOUSE:
+            idd, x, y, z, bstate = curses.getmouse()
+            screen.move(y, x)
+            screen.addstr(f"y: {y}, x: {x}")
+        else:
+            screen.addstr("ting:" + chr(ch))
+
+os.environ.setdefault("ESCDELAY", "25")
+curses.wrapper(mouseTest)
